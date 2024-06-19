765e0f3e54e896fc9568a68520118b5b37a2ce09
Krissada Singhakachain <46844213+OmsinKrissada@users.noreply.github.com>
2024-06-19 16:35:50 +0700

    added toggle for anti-peaking in settings

--- src/modules/antipeaking.ts
+++ src/modules/antipeaking.ts
@@ -1,9 +1,10 @@
+import { getGuildSettings } from "../DBManager";
 import { logger } from "../Logger";
 import { bot } from "../Main";
 
 const joinTime = new Map<string, number>();
 
-bot.on('voiceStateUpdate', (oldState, newState) => {
+bot.on("voiceStateUpdate", async (oldState, newState) => {
 	// on join
 	if (!oldState.channel && newState.channel) {
 		joinTime.set(oldState.member.id, new Date().valueOf());
@@ -12,15 +13,17 @@ bot.on('voiceStateUpdate', (oldState, newState) => {
 	if (oldState.channel && !newState.channel) {
 		const joinAt = joinTime.get(oldState.member.id);
 		const leaveAt = new Date().valueOf();
-		if (joinAt && leaveAt - joinAt < 5000 && oldState.channel.members.size > 0) {
-			oldState.guild.systemChannel.send({
-				embed: {
-					description: `${oldState.member} peaked in <#${oldState.channel.id}> for **${Math.round((leaveAt - joinAt) / 100) / 10} s**`
-				}
-			}).catch(err => {
-				logger.warn(`Cannot send anti-peak message to channel ${oldState.guild.systemChannel} of ${oldState.guild.id}: ${err}`);
-
-			});
+		const { antiPeaking: enabled } = await getGuildSettings(oldState.guild.id);
+		if (enabled && joinAt && leaveAt - joinAt < 5000 && oldState.channel.members.size > 0) {
+			oldState.guild.systemChannel
+				.send({
+					embed: {
+						description: `${oldState.member} peaked in <#${oldState.channel.id}> for **${Math.round((leaveAt - joinAt) / 100) / 10} s**`,
+					},
+				})
+				.catch((err) => {
+					logger.warn(`Cannot send anti-peak message to channel ${oldState.guild.systemChannel} of ${oldState.guild.id}: ${err}`);
+				});
 		}
 	}
 });
